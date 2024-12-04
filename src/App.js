d16749911492f911adede7749ed25471a699cae3
Gursimran Singh <gursimransingh1331@gmail.com>
2024-12-04 05:31:34 -0500

    Initial Commit Changes in App.js File

--- src/App.js
+++ src/App.js
@@ -5,18 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Group Leader - Parbhat Sharma </h1>
+       <h1>Gursimran Singh</h1>
+       <h1>Simarpreet Sandhu</h1>
+       <h1>Jaspreet Singh</h1>
+       <h1>Rajat Sareen</h1>
       </header>
     </div>
   );
